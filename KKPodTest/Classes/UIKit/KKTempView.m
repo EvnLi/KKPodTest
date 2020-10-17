7cc40ee577bcf20f0a4e1740f4ce74ef00f6675c
Mac <953975027@qq.com>
2020-10-17 14:33:02 +0800

    添加日志测试

--- KKPodTest/Classes/UIKit/KKTempView.m
+++ KKPodTest/Classes/UIKit/KKTempView.m
@@ -9,12 +9,8 @@
 
 @implementation KKTempView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
++ (void)log {
+    NSLog("测试Pod");
 }
-*/
 
 @end
